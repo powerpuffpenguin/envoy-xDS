5781b7ccdd9ca91ea0e77a803b227c905b715edd
zuiwuchang <zuiwuchang@gmail.com>
2025-09-18 11:10:40 +0800

    listener

--- listener.ts
+++ listener.ts
@@ -4,7 +4,7 @@ import type { NameDS } from "./provider.ts";
 
 export interface ListenerOptions {
     /**
-     * cluster name, must be unique
+     * listen name, must be unique
      */
     name: string
     /**
@@ -19,6 +19,16 @@ export interface ListenerOptions {
      */
     network?: 'tcp' | 'unix'
 
+    /**
+     * Specifying routes for http.
+     * When both http and https are specified, http will be ignored.
+     */
+    http?: NameDS,
+    /**
+     * Specifying routes for https.
+     * When both http and https are specified, http will be ignored.
+     */
+    https?: NameDS[],
     /**
      * default xDS
      */
@@ -39,21 +49,107 @@ export class Listener implements NameDS {
         return this.opts.name
     }
     constructor(opts: ListenerOptions) {
+        if (opts.name == '') {
+            throw new Error('listener name invalid')
+        }
+        const network = opts.network ?? 'tcp'
+        switch (network) {
+            case 'tcp':
+            case 'unix':
+                break
+            default:
+                throw new Error(`listener unknow network: ${network}`)
+        }
+
         this.opts = { ...opts }
         this.init = { ...opts.init ?? {} }
         this.overlay = { ...opts.overlay ?? {} }
-        this.toJSON()
     }
 
-    toJSON(): Record<string, any> {
+    async toJSON() {
         const opts = this.opts
-        if (opts.name == '') {
-            throw new Error('cluster name invalid')
+        const router: Record<string, any> = {}
+        if (opts.https) {
+            router['filter_chains'] = await this._filter_chains()
+            router['listener_filters'] = [
+                {
+                    "name": "envoy.filters.listener.tls_inspector",
+                    "typed_config": {
+                        "@type": "type.googleapis.com/envoy.extensions.filters.listener.tls_inspector.v3.TlsInspector"
+                    }
+                }
+            ]
+        } else {
+            router['default_filter_chain'] = await this._default_filter_chain()
         }
         return {
             ...this.init,
-            "@type": "type.googleapis.com/envoy.config.cluster.v3.Cluster",
+            '@type': "type.googleapis.com/envoy.config.listener.v3.Listener",
+            name: 'lds_' + opts.name,
+            address: this._address(),
+            ...router,
             ...this.overlay,
+        }
+    }
+    private async _default_filter_chain() {
+        const route = this.opts.http
+        return {
+            "filters": [
+                {
+                    "name": "envoy.filters.network.http_connection_manager",
+                    "typed_config": {
+                        "@type": "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager",
+                        "access_log": {
+                            "name": "envoy.access_loggers.stdout",
+                            "typed_config": {
+                                "@type": "type.googleapis.com/envoy.extensions.access_loggers.stream.v3.StdoutAccessLog"
+                            }
+                        },
+                        "generate_request_id": false,
+                        "http_filters": [
+                            {
+                                "name": "envoy.filters.http.router",
+                                "typed_config": {
+                                    "@type": "type.googleapis.com/envoy.extensions.filters.http.router.v3.Router"
+                                }
+                            }
+                        ],
+                        "route_config": route ? await route.toJSON() : undefined,
+                        "stat_prefix": route ? 'route_' + route.name : undefined,
+                        "strip_any_host_port": true
+                    }
+                }
+            ]
+        }
+
+    }
+    private async _filter_chains() {
+        const https = this.opts.https ?? []
+        const chains = new Array(https.length)
+        for (let i = 0; i < https.length; i++) {
+            chains[i] = await https[i].toJSON()
+        }
+        return chains
+    }
+    private _address() {
+        const opts = this.opts
+        const network = opts.network ?? 'tcp'
+        switch (network) {
+            case 'tcp':
+                break
+            case 'unix':
+                return {
+                    pipe: {
+                        path: opts.addr,
+                    }
+                }
+        }
+        const [host, port] = splitHostPort(opts.addr)
+        return {
+            socket_address: {
+                address: host === '' ? '::' : host,
+                port_value: port,
+            }
         }
     }
 }
